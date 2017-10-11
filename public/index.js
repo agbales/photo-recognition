b06e0a884fafe90aac9bd41bf05b47cb2f76bc7f
Andrew Bales <agbales@gmail.com>
2017-10-10 19:27:31 -0700

    added bootstrap, loading screen

--- public/index.js
+++ public/index.js
@@ -2,15 +2,13 @@ $(document).ready(function(){
   var socket = io.connect();
 
   socket.on('data', function(msg) {
-    console.log('got data message...', msg);
-
     $('#info').html('<img src="' + msg.uploaded_image_url + '" width="300px">');
 
     var attributes = msg.images[0].faces[0].attributes;
     for (var prop in attributes) {
       if (prop == "gender") {
         var g = attributes[prop];
-        $('#info').append($('<div>').text("Gender: " + g.type + "(female confidence: " + g.femaleConfidence  + ", male confidence: " + g.maleConfidence + ")"));
+        $('#info').append($('<div>').text("Gender: " + g.type + " (female confidence: " + g.femaleConfidence  + ", male confidence: " + g.maleConfidence + ")"));
       } else {
         var info = prop + ": " + attributes[prop];
         $('#info').append($('<div>').text(info));
@@ -21,6 +19,7 @@ $(document).ready(function(){
 
   $('#url-submit-form').submit(function() {
     socket.emit('url submit', $('#input').val());
+    $('#info').html('<div>loading . . .</div>');
     return false;
   });
 });
